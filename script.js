03ff8067f7abe6b51db65fb3a241a417058cf489
azizaweb <azizaweb@mail.ru>
2025-01-22 19:40:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// let fruits = ['яблоко', 'груша', 'слива'];
+// console.log(fruits);
+// console.log(fruits[1])
+//  с нуля начинается
+// console.log(fruits[2] = 'лимон');
+// console.log(fruits) изменить значение
+// console.log(fruits[3] = 'вишня');
+// console.log(fruits) добавить значение
+// console.log(fruits.at(-1)) выводит последнее  значение
+
+//                            методы эрей
+// // push shift unshift pop нач array
+// let fruits = ['яблоко', 'груша', 'слива'];
+// console.log(fruits.pop())
+// console.log(fruits) удаляет с самого конца
+// let fruits = ['яблоко', 'груша', 'слива'];
+//  console.log(fruits.push('Лимон'))
+//  console.log(fruits) добавляет с самого конца
+// let fruits = ['яблоко', 'груша', 'слива'];
+//  console.log(fruits.unshift('Лимон'))
+// console.log(fruits) 
+// доб с самого начала
+// let fruits = ['яблоко', 'груша', 'слива'];
+//  console.log(fruits.shift('Лимон'))
+// console.log(fruits) удаляет с самого начала
+
+// let people = ['Ismail','Samira', 'Mirana' ];
+// console.log(people);
+// console.log(people.push('Madina'))
+// console.log(fruits)
+// console.log(people.unshift('Leyli'))
+//  console.log(people)
+
+let chocolate = ['snikers','twix', 'kitkat','mars' ];
+console.log(chocolate.length)
+alert(chocolate.length);
+if(confirm('удалить один элемент?')){
+    console.log(chocolate.pop())
+    console.log('удален')
+}else{
+    console.log('остались ')
+}
+console.log ('chocolate')
+
+
+
